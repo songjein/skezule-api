6a8a0b4d7ff4ef0ce13023ca6c4ed41c37041a70
songjein <jeinsong100@gmail.com>
2017-07-16 12:46:47 +0000

    users controller remove unnecessary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,7 @@ class UsersController < ApplicationController
 	require 'digest/sha1'
 
 	def show
-		#user = User.find_by(id: params[:id])
-		#render json: user
-		render json: {message: "login", user: session[:user_id]}
+		render json: @current_user 
 	end
 	
 	def create 
@@ -14,37 +12,7 @@ class UsersController < ApplicationController
 		user.salt = BCrypt::Engine.generate_salt
 		user.password = BCrypt::Engine.hash_secret(params[:password], user.salt)
 		user.save
-		
 		render json: user
 	end
 
-	def login
-		uid = params[:uid]
-		user = User.find_by(uid: uid)
-		if !user
-			render json: {message: 'no id'}
-			return
-		end
-		password = BCrypt::Engine.hash_secret(params[:password], user.salt)
-
-		if user 
-			if user.password == password	
-				# 로그인
-				session[:user_id] = user.id
-				render json: {message: session[:user_id]}
-			else
-				# 비번 틀림
-				render json: {message: 'pw'}
-			end
-		else 
-			# 아디 존재 x
-			render json: {message: 'id'}
-		end	
-	end
-
-	def logout 
-		if session[:user_id] 
-			session.delete(:user_id)
-		end
-	end
 end
